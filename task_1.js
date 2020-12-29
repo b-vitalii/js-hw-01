fe6870c60ef64efc600ee2e96aa1223f1ea0fcfd
Vitalii Boiko <74602993+b-vitalii@users.noreply.github.com>
2020-12-29 18:42:13 +0200

    Add files via upload

---
+++ task_1.js
@@ -0,0 +1,8 @@
+let name = 'protective field generator';
+let price = 1000;
+
+console.log('Selected' + name + 'per' + price + 'credits for one');
+
+let newPrice = 2000;
+
+console.log('Selected' + name + 'per' + newPrice + 'credits for one');
