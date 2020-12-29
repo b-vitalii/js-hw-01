d83582cea425307a4f2e2dd93a6f945e499da735
b-vitalii <vitali90@ukr.net>
2020-12-29 19:17:18 +0200

    myFirst

---
+++ task_1.js
@@ -0,0 +1,10 @@
+let name = 'protective field generator';
+let price = 1000;
+
+console.log(`Selected ${name} per ${price} credits for one`);
+
+let newPrice = name + 1000;
+
+console.log(`Selected ${name} per ${newPrice} credits for one`);
+
+// my first lesson
