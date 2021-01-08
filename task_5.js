c9905928c796c3e3b00ccdb46fa990d93d259b55
b-vitalii <vitali90@ukr.net>
2021-01-08 18:49:42 +0200

    new

--- task_5.js
+++ task_5.js
@@ -1,6 +1,6 @@
 //My fifth lesson
 
-let massage = prompt("Enter your country for delivery").toUpperCase();
+let massage = prompt("Enter your country for delivery");
 let priceChina = 150;
 let priceChili = 250;
 let priceAustralia = 165;
@@ -8,21 +8,21 @@ let priceIndia = 90;
 let priceJamaica = 130;
 
 // Use switch
-switch (massage) {
+switch (massage.toUpperCase()) {
     case `CHINA`:
-        alert(`Shipping to ${massage} will cost ${priceChina} credits`)
+        alert(`Shipping to ${massage.charAt(0).toUpperCase() + massage.slice(1).toLowerCase()} will cost ${priceChina} credits`)
         break;
     case `CHILI`:
-        alert(`Shipping to ${massage} will cost ${priceChili} credits`)
+        alert(`Shipping to ${massage[0].toUpperCase() + massage.slice(1).toLowerCase()} will cost ${priceChili} credits`)
         break;
     case `AUSTRALIA`:
-        alert(`Shipping to ${massage} will cost ${priceAustralia} credits`)
+        alert(`Shipping to ${massage[0].toUpperCase() + massage.slice(1).toLowerCase()} will cost ${priceAustralia} credits`)
         break;
     case `INDIA`:
-        alert(`Shipping to ${massage} will cost ${priceIndia} credits`)
+        alert(`Shipping to ${massage[0].toUpperCase() + massage.slice(1).toLowerCase()} will cost ${priceIndia} credits`)
         break;
     case `JAMAICA`:
-        alert(`Shipping to ${massage} will cost ${priceJamaica} credits`)
+        alert(`Shipping to ${massage[0].toUpperCase() + massage.slice(1).toLowerCase()} will cost ${priceJamaica} credits`)
         break;
     case null:
         alert(`Canceled by user!`)
